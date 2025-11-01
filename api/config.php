b6633fd373e92b19e42b7c41441d775413eb9895
Miao King <bjonline@qq.com>
2025-11-02 02:22:05 +0800

    Create config.php

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+// 数据库配置文件 - 请勿提交到版本控制系统
+// 注意：请根据实际环境修改以下配置信息
+
+// 数据库连接信息
+return array(
+    'host' => '192.168.0.88',       // 数据库主机IP（如群晖NAS的IP）
+    'port' => 3307,                 // 数据库端口
+    'db_name' => 'Books',         // 数据库名
+    'username' => 'phpuser',        // 数据库用户名
+    'password' => 'your_password'   // 数据库密码
+);
